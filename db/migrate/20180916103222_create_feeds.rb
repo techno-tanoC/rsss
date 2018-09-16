e24df5f4926795d66cd9a9dc244d90e75ad014e1
techno-tanoC <develop.tanoc@gmail.com>
2018-09-16 19:32:29 +0900

    generate feed scaffold

---
+++ db/migrate/20180916103222_create_feeds.rb
@@ -0,0 +1,10 @@
+class CreateFeeds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feeds do |t|
+      t.text :title
+      t.text :url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/feeds.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :feed do
+    title { "MyText" }
+    url { "MyText" }
+  end
+end
