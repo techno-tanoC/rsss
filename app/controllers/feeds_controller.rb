fc28c41869b830d2b53221742de9dcef44e260f7
techno-tanoC <develop.tanoc@gmail.com>
2018-09-16 20:28:26 +0900

    update comments on controllers

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -62,12 +62,10 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_feed
       @feed = Feed.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
       params.require(:feed).permit(:title, :url)
     end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
 
-  # GET /items
-  # GET /items.json
+  # GET /feeds/:feed_id/items
+  # GET /feeds/:feed_id/items.json
   def index
     @feed = Feed.find(params[:feed_id])
     @items = @feed.items
@@ -14,12 +14,10 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:title, :url, :published_at, :feed_id)
     end
