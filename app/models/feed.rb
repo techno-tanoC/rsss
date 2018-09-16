6d51facc6f96f084eeb175b21a50b82cb5f31b10
techno-tanoC <develop.tanoc@gmail.com>
2018-09-16 21:53:49 +0900

    add Feed#fetch_and_update

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,11 @@
 class Feed < ApplicationRecord
   has_many :items
+
+  def fetch_and_update
+    feed = Feedjira::Feed.fetch_and_parse(self.url)
+    feed.entries.each do |entry|
+      item = self.items.find_or_initialize_by(url: entry.url)
+      item.update(title: entry.title, published_at: entry.published)
+    end
+  end
 end
