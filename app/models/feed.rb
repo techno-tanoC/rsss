af26651d860000ebe5235a2f96da5786d73021dd
techno-tanoC <develop.tanoc@gmail.com>
2018-09-16 23:49:19 +0900

    fix Feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,7 +3,7 @@ class Feed < ApplicationRecord
 
   def fetch_and_update
     feed = Feedjira::Feed.fetch_and_parse(self.url)
-    feed.entries.each do |entry|
+    feed.entries.reverse_each do |entry|
       item = self.items.find_or_initialize_by(url: entry.url)
       item.update(title: entry.title, published_at: entry.published)
     end
